bb8ec432e40ad86b265ec573fc57b7f65c06ce81
Antonius Danu Pranoto <antoniusdanu600@gmail.com>
2026-06-02 11:21:15 +0700

    Update Lora_tx.ino

--- Lora_tx.ino
+++ Lora_tx.ino
@@ -1,43 +1,36 @@
-
 #include <SPI.h>
 #include <LoRa.h>
 
-//define the pins used by the transceiver module
-#define ss 5
-#define rst 14
-#define dio0 2
+#define SS_PIN    5
+#define RST_PIN   14
+#define DIO0_PIN  26
 
 int counter = 0;
 
 void setup() {
-  //initialize Serial Monitor
+
   Serial.begin(115200);
-  while (!Serial);
+
   Serial.println("LoRa Sender");
 
-  //setup LoRa transceiver module
-  LoRa.setPins(ss, rst, dio0);
-  
-  //replace the LoRa.begin(---E-) argument with your location's frequency 
-  //433E6 for Asia
-  //868E6 for Europe
-  //915E6 for North America
-  while (!LoRa.begin(868E6)) {
-    Serial.println(".");
-    delay(500);
+  LoRa.setPins(SS_PIN, RST_PIN, DIO0_PIN);
+
+  while (!LoRa.begin(433E6)) {
+
+    Serial.println("LoRa init failed...");
+    delay(1000);
   }
-   // Change sync word (0xF3) to match the receiver
-  // The sync word assures you don't get LoRa messages from other LoRa transceivers
-  // ranges from 0-0xFF
+
   LoRa.setSyncWord(0xF3);
+
   Serial.println("LoRa Initializing OK!");
 }
 
 void loop() {
-  Serial.print("Sending packet: ");
+
+  Serial.print("Sending packet ");
   Serial.println(counter);
 
-  //Send LoRa packet to receiver
   LoRa.beginPacket();
   LoRa.print("hello ");
   LoRa.print(counter);
@@ -45,5 +38,5 @@ void loop() {
 
   counter++;
 
-  delay(10000);
+  delay(2000);
 }
